32042afa6088a7ae6e37d5811e47f55bb11dd509
Neka Kerr <anneika.kerr@gmail.com>
2020-09-26 07:06:33 -0400

    Users table added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_160605) do
+ActiveRecord::Schema.define(version: 2020_09_26_105856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 
   create_table "wall_rental_requests", force: :cascade do |t|
     t.integer "wall_id"
