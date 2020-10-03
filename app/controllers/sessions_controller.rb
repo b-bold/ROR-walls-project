6123294b90f2a925fba2c77112350842fbc5ee2b
Neka Kerr <anneika.kerr@gmail.com>
2020-10-02 21:07:02 -0400

    login and logout buttons work as intended.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
         return nil if current_user.nil?
         current_user.reset_session_token!
         session[:session_token] = nil
-        redirect_to walls_url
+        redirect_to new_session_url
     end 
 
 
